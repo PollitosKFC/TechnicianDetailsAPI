cc9f4a2a55d61770c2f9f3fdc76d9bbdce91a15c
ShoLee01 <miguelalfaro2017@gmail.com>
2022-05-05 14:03:57 -0500

    feat: creating proyect

---
+++ src/main/java/pe/edu/upc/techniciandetailsapi/Easyjobs/EasyjobsApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.techniciandetailsapi.Easyjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyjobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/techniciandetailsapi/Easyjobs/EasyjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.techniciandetailsapi.Easyjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
